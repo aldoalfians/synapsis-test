0fbd8e2e93603d58c80b46209e2cabc027334725
aldoalfians <aldoboetd908@gmail.com>
2024-12-04 09:13:53 +0700

    fix: setting query client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,18 @@ import type { AppProps } from "next/app";
 import { ConfigProvider } from "antd";
 import themeConfig from "@/theme/themeConfig";
 import "antd/dist/reset.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <ConfigProvider theme={themeConfig}>
-        <Component {...pageProps} />
-      </ConfigProvider>
+      <QueryClientProvider client={queryClient}>
+        <ConfigProvider theme={themeConfig}>
+          <Component {...pageProps} />
+        </ConfigProvider>
+      </QueryClientProvider>
     </>
   );
 }
