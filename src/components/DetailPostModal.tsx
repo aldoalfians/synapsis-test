54c72150845d356c0a396fe0ce35b2898d152da1
aldoalfians <aldoboetd908@gmail.com>
2024-12-04 20:33:36 +0700

    feat: add detail modal

---
+++ src/components/DetailPostModal.tsx
@@ -0,0 +1,37 @@
+import useQueryFetch from "@/utilities/hooks/useQueryFetch";
+import { Modal, Skeleton, Spin, Typography } from "antd";
+
+const { Title, Text } = Typography;
+
+interface Props {
+  isOpen?: boolean;
+  slug?: string | number;
+  onClose?: () => void;
+}
+
+function DetailPostModal({ onClose, isOpen, slug }: Props) {
+  const { data, isLoading } = useQueryFetch({
+    url: `posts/${slug}`,
+    enabled: Boolean(isOpen),
+  });
+
+  return (
+    <Modal
+      title="Detail Post"
+      open={isOpen}
+      onOk={onClose}
+      footer={null}
+      onCancel={onClose}
+      width={500}
+      centered
+      destroyOnClose
+    >
+      <Spin spinning={isLoading}>
+        <Title level={4}>{data?.title || "-"}</Title>
+        <Text>{data?.body || "-"}</Text>
+      </Spin>
+    </Modal>
+  );
+}
+
+export default DetailPostModal;
