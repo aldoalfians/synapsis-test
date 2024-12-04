e3d119370fb1d43be653a8fbdbd67c18a446c719
aldoalfians <aldoboetd908@gmail.com>
2024-12-04 20:33:43 +0700

    fix: update styling card

--- src/components/BaseCard.tsx
+++ src/components/BaseCard.tsx
@@ -1,6 +1,18 @@
-import { Button, Card, Dropdown, MenuProps, Row, Typography } from "antd";
+import {
+  Button,
+  Card,
+  Col,
+  Divider,
+  Dropdown,
+  MenuProps,
+  Row,
+  Space,
+  Typography,
+} from "antd";
 import { EllipsisOutlined } from "@ant-design/icons";
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import { useRouter } from "next/router";
+import usePostMutator from "@/utilities/hooks/posts/usePostMutator";
 
 const { Title, Paragraph } = Typography;
 
@@ -8,39 +20,81 @@ interface Props {
   title?: string;
   body?: string;
   postId?: string | number;
+  onClick?: () => void;
 }
 
-const items: MenuProps["items"] = [
-  {
-    key: "1",
-    label: "Hapus Post",
-  },
-];
+function BaseCard({ title, body, postId, onClick }: Props) {
+  const router = useRouter();
+  const { submitDeletePost, isLoadingDeletePost } = usePostMutator({
+    id: postId,
+  });
 
-function BaseCard({ title, body, postId }: Props) {
-  const [isOpenDelete, setIsOpenDelete] = useState<boolean>(false);
+  const items: MenuProps["items"] = [
+    {
+      key: "1",
+      label: "Hapus Post",
+    },
+    {
+      key: "2",
+      label: "Edit Post",
+    },
+  ];
+
+  const handleMenuClick: MenuProps["onClick"] = useCallback(
+    ({ key }: { key: string }) => {
+      if (key === "1") {
+        submitDeletePost && submitDeletePost({});
+      } else if (key === "2") {
+        router.push(`/edit/${postId}`);
+      }
+    },
+    [postId, router]
+  );
 
   return (
-    <Card size="small" hoverable>
-      <Row justify="space-between">
-        <Title level={5}>#{postId || 0}</Title>
-        <Dropdown
-          menu={{
-            items,
-          }}
-          placement="bottomRight"
-          arrow={{
-            pointAtCenter: true,
-          }}
-        >
-          <Button type="text" icon={<EllipsisOutlined />} />
-        </Dropdown>
+    <Card size="small" hoverable style={{ minHeight: 216 }}>
+      <Row justify="space-between" align="middle">
+        <Col>
+          <Title level={5}>#{postId || 0}</Title>
+        </Col>
+        <Col>
+          <Space>
+            <Dropdown
+              menu={{
+                items,
+                onClick: handleMenuClick,
+              }}
+              placement="bottomRight"
+              arrow={{
+                pointAtCenter: true,
+              }}
+            >
+              <Button
+                size="small"
+                type="default"
+                icon={<EllipsisOutlined />}
+                disabled={isLoadingDeletePost}
+              />
+            </Dropdown>
+          </Space>
+        </Col>
       </Row>
+
+      <Divider orientation="center" style={{ margin: "12px 0" }} />
 
       <Title level={2} ellipsis>
         {title || "-"}
       </Title>
       <Paragraph ellipsis={{ rows: 2 }}>{body || "-"}</Paragraph>
+
+      <Button
+        style={{ fontSize: 12 }}
+        size="small"
+        onClick={onClick}
+        disabled={isLoadingDeletePost}
+      >
+        Selengkapnya
+      </Button>
     </Card>
   );
 }
